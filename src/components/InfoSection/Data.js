75ad3683557f6ca9ec15e6aa97d4bdb477ca524a
AdroitMachine <anujsaxenamail@gmail.com>
2020-10-19 15:02:15 +0530

    initial commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get accesss to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: true,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited access',
+    headline: 'Another paragraph with some text',
+    description: 'Paragraph enter the text here.........................................................',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our team',
+    headline: 'Headline Number three',
+    description: 'Paragraph placeholder put some text here........................................',
+    buttonLabel: 'Start now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+}
